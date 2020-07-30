b3d38c5bd84663ad55594700ca51c6c4a58285ee
MElizabeth-Rios <65927929+MElizabeth-Rios@users.noreply.github.com>
2020-07-29 23:26:21 -0500

    Initial commit

---
+++ Assets/Script/Kevin.cs
@@ -0,0 +1,49 @@
+﻿//Nombre del desarrollador: María Elizabeth Rios Linderos
+//Asignatura: Estructura de datos
+//Descripcion del uso de este codigo: 
+/*
+ Este script se utilizara para generar el control del avatar jugador 
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kevin : MonoBehaviour
+{// inicio del cuerpo de la clase
+    Rigidbody2D movimientos2D;
+    public int vida;
+    public float saltoPersonaje;
+    [SerializeField]
+    float velocidad;
+    public SpriteRenderer spriteKevin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movimientos2D = GetComponent<Rigidbody2D>();
+        spriteKevin = GetComponent<SpriteRenderer>();
+        saltoPersonaje = 5.0f;
+        velocidad = 3.0f;
+        vida = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            movimientos2D.AddForce(Vector2.up * saltoPersonaje, ForceMode2D.Impulse);
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) 
+        {
+            movimientos2D.velocity = new Vector2(velocidad, movimientos2D.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            movimientos2D.velocity = new Vector2(-velocidad, movimientos2D.velocity.y);
+        }
+        if (vida<=0)
+        {
+            spriteKevin.enabled = false;
+        }
+    }
+}// fin del cuerpo de la clase
---
+++ Assets/Script/Sensor.cs
@@ -0,0 +1,30 @@
+﻿//Nombre del desarrollador: María Elizabeth Rios Linderos
+//Asignatura: Estructura de datos
+//Descripcion del uso de este codigo: 
+/*
+ Este script funcionará para similar la destrucción de un objeto al ser colisionado por otro  
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sensor : MonoBehaviour
+{//inicio del cuerpo de la clase
+    [SerializeField]
+    Kevin vidaKevin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vidaKevin = GameObject.FindGameObjectWithTag("Player").GetComponent<Kevin>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //Debug.Log("Hola");
+            vidaKevin.vida--;
+        }
+    }
+    
+}//fin del cuerpo de la clase 
